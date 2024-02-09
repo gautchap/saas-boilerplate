3a53f26474ada5ea42490262b38f651de35be6ce
gautchap <gautchap+github@outlook.com>
2024-02-09 14:39:55 +0100

    feat : add profile avatar

---
+++ src/components/profile.tsx
@@ -0,0 +1,36 @@
+import { Button } from "@/components/ui/button";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import Link from "next/link";
+import { Session } from "next-auth";
+import LogoutButton from "@/components/logout-button";
+import Avvvatars from "avvvatars-react";
+
+export default function Profile({ session }: { session: Session }) {
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button className="rounded-full bg-muted" variant="ghost" size="icon">
+                    {session.user?.image ? (
+                        <Avatar>
+                            <AvatarImage src={session.user?.image} className="p-1" />
+                        </Avatar>
+                    ) : (
+                        <Avvvatars value={session.user.email} />
+                    )}
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+                <DropdownMenuItem className="flex cursor-pointer items-center justify-between">
+                    <Link href="/me">My account</Link>
+                </DropdownMenuItem>
+                <LogoutButton />
+            </DropdownMenuContent>
+        </DropdownMenu>
+    );
+}
