9b6f1c85c2a61d34d2be5783bacbbef0f439d0bf
gautchap <gautchap+github@outlook.com>
2024-02-09 14:39:26 +0100

    feat : create modal to log in

---
+++ src/components/modal-login.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import { useMediaQuery } from "@/hooks/useMediaQuery";
+import { Button } from "@/components/ui/button";
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+    Drawer,
+    DrawerClose,
+    DrawerContent,
+    DrawerDescription,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerTitle,
+    DrawerTrigger,
+} from "@/components/ui/drawer";
+import { useState } from "react";
+import { LoginForm } from "@/components/login-form";
+
+export function ModalLogin({ providers }: { providers: string[] }) {
+    const [open, setOpen] = useState(false);
+    const isDesktop = useMediaQuery("(min-width: 768px)");
+
+    if (isDesktop) {
+        return (
+            <Dialog open={open} onOpenChange={setOpen}>
+                <DialogTrigger asChild>
+                    <Button>Sign In</Button>
+                </DialogTrigger>
+                <DialogContent className="sm:max-w-[425px]">
+                    <DialogHeader>
+                        <DialogTitle>Sign In</DialogTitle>
+                        <DialogDescription>Enter your mail to connect</DialogDescription>
+                    </DialogHeader>
+                    <LoginForm providers={providers} />
+                </DialogContent>
+            </Dialog>
+        );
+    }
+
+    return (
+        <Drawer open={open} onOpenChange={setOpen}>
+            <DrawerTrigger asChild>
+                <Button>Sign In</Button>
+            </DrawerTrigger>
+            <DrawerContent className="px-4">
+                <DrawerHeader className="text-left">
+                    <DrawerTitle>Sign In</DrawerTitle>
+                    <DrawerDescription>Enter your mail to connect</DrawerDescription>
+                </DrawerHeader>
+                <LoginForm providers={providers} />
+                <DrawerFooter className="pt-2">
+                    <DrawerClose asChild>
+                        <Button variant="outline">Close</Button>
+                    </DrawerClose>
+                </DrawerFooter>
+            </DrawerContent>
+        </Drawer>
+    );
+}
