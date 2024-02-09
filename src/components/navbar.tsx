b66ad851043af61d2bd7c7d3556c9ac43eb1f0cd
gautchap <gautchap+github@outlook.com>
2024-02-09 14:40:15 +0100

    refactor : get list of providers

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,17 +3,22 @@ import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
 import MobileNav from "@/components/mobile-nav";
 import ThemeToggle from "@/components/theme-toggle";
-import LoginButton from "@/components/login-button";
+import { ModalLogin } from "./modal-login";
+import { getProviders } from "next-auth/react";
+import Profile from "@/components/profile";
 
 export default async function Navbar() {
     const session = await getServerSession(authOptions);
+    const AllProviders = await getProviders();
+    const providers = Object.keys(AllProviders || {});
+
     return (
         <>
             <div className="container flex items-center justify-between py-2">
                 <DesktopNav />
                 <MobileNav />
                 <div className="flex items-center gap-2">
-                    <LoginButton session={session} />
+                    {session ? <Profile session={session} /> : <ModalLogin providers={providers} />}
                     <ThemeToggle />
                 </div>
             </div>
