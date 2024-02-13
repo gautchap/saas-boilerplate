cf15bf5b609afab8d11b8e0260fe9fcca361da59
gautchap <gautchap+github@outlook.com>
2024-02-13 14:12:37 +0100

    feat : add nodemailer to send auth email

--- src/template/mail.ts
+++ src/template/mail.ts
@@ -1,10 +1,47 @@
-import { Resend } from "resend";
+import { createTransport } from "nodemailer";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+type verificationRequestParameters = {
+    identifier: string;
+    url: string;
+    provider: {
+        server: {
+            host: string;
+            port: number;
+            auth: {
+                user: string;
+                pass: string;
+            };
+        };
+        from: string;
+    };
+};
 
-resend.emails.send({
-    from: "onboarding@resend.dev",
-    to: "gauchap+github@outlook.com",
-    subject: "Hello World",
-    html: "<p>Congrats on sending your <strong>first email</strong>!</p>",
-});
+export async function sendVerificationRequest({ identifier, url, provider }: verificationRequestParameters) {
+    const { host } = new URL(url);
+    const transport = createTransport(provider.server);
+    const result = await transport.sendMail({
+        to: identifier,
+        from: {
+            name: "My SaaS App",
+            address: provider.from,
+        },
+        subject: `Sign in to ${host}`,
+        text: text({ url, host }),
+        html: html({ url, host }),
+    });
+    const failed = result.rejected.concat(result.pending).filter(Boolean);
+    if (failed.length > 0) {
+        throw new Error(`Email(s) (${failed.join(", ")}) could not be sent`);
+    }
+}
+
+function html(parameters: { url: string; host: string }) {
+    const { url } = parameters;
+
+    return `Oh ! Here is your magic link : <a href="${url}">Link</a>`;
+}
+
+/** Email Text body (fallback for email clients that don't render HTML, e.g. feature phones) */
+function text({ url, host }: { url: string; host: string }) {
+    return `Sign in to ${host}\n${url}\n\n`;
+}
