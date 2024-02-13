a3fe3ad4b624c53935e2f325dadaeeb89fe84606
gautchap <gautchap+github@outlook.com>
2024-02-13 13:42:55 +0100

    feat : add mail template

--- src/types/envSchema.ts
+++ src/types/envSchema.ts
@@ -11,6 +11,7 @@ const envSchema = z.object({
     DATABASE_URL: z.string().trim().min(1),
     GITHUB_CLIENT_ID: z.string().trim().min(1),
     GITHUB_CLIENT_SECRET: z.string().trim().min(1),
+    RESEND_API_KEY: z.string().trim().min(1),
 });
 
 const envServer = envSchema.safeParse({
@@ -24,6 +25,7 @@ const envServer = envSchema.safeParse({
     DATABASE_URL: process.env.DATABASE_URL,
     GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
     GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
+    RESEND_API_KEY: process.env.RESEND_API_KEY,
 });
 
 if (!envServer.success) {
